f28bb5b1dfb271d30726ea90a90baaef7e5ecf12
Vaibhav050-rx <vaibhavjain7925@gmail.com>
2026-05-01 01:24:20 +0530

    Time: 0 ms (100%), Space: 42.8 MB (56.01%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[]dp = new int [n+1];
-        Arrays.fill(dp,-1);
-        return MaxOpt(nums,n-1,dp);
-    }
-    public static int MaxOpt(int[]nums,int idx,int[]dp){
-        // base case 
-        if(idx==0) return nums[0];
-        if(idx<0) return 0;
-        // check 
-        if(dp[idx]!=-1) return dp[idx];
-
-        int pick = nums[idx]+MaxOpt(nums,idx-2,dp);
-        int NotPick =0+MaxOpt(nums,idx-1,dp);
-        dp[idx]=Math.max(pick,NotPick);
-        return dp[idx];
+           
+        int prev = nums[0]; // dp[i-1]
+        int prev2 = 0; // dp[i-2]
+        for (int i = 1; i < n; i++) {
+            int take = nums[i];
+            if (i > 1) take += prev2;
+            int notTake = 0+ prev;
+            int cur = Math.max(take, notTake);
+            // shift window
+            prev2 = prev;
+            prev = cur;
+        }
+        return prev;
     }
 }

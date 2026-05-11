96466a16d59a3ffad2b96dbb4fff44b5084601b7
Vaibhav050-rx <vaibhavjain7925@gmail.com>
2026-05-11 19:26:44 +0530

    Time: 14 ms (90.1%), Space: 47 MB (24.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,28 +1,31 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int[][]dp=new int[n][amount+1];
-        for(int[]rows:dp){
-            Arrays.fill(rows,-1);
+        int[]prev=new int[amount+1];
+        // Base Case
+        for(int tar = 0; tar <= amount; tar++) {
+            if(tar % coins[0] == 0) {
+                prev[tar] = tar / coins[0];
+            }
+            else {
+                prev[tar] = (int)1e9;
+            }
         }
-        int ans =MinCoin(coins,n-1,amount,dp);
-        if(ans>=1e9) return -1;
+        // Build DP Table
+        for(int i = 1; i < n; i++) {
+            int[]curr=new int[amount+1];
+            for(int j = 0; j <= amount; j++) {
+                int notTake = prev[j];
+                int take = (int)1e9;
+                if(coins[i] <= j) {
+                    take = 1 +curr[j - coins[i]];
+                }
+                curr[j] =Math.min(take, notTake);
+            }
+            prev=curr;
+        }
+        int ans = prev[amount];
+        if(ans >= 1e9) return -1;
         return ans;
-    }
-    public static int MinCoin(int[]arr,int i,int amount,int[][]dp){
-        // base case 
-        if(amount == 0) return 0;
-        if(i==0){
-            if(amount % arr[0]==0) return amount/arr[0];
-            else return (int)1e9;
-        }
-        if(dp[i][amount]!=-1) return dp[i][amount];
-        //explore all paths
-        int notPick = MinCoin(arr,i-1,amount,dp);
-        int pick=Integer.MAX_VALUE;
-        if(arr[i]<=amount){
-            pick=1+MinCoin(arr,i,amount-arr[i],dp);
-        }
-        return dp[i][amount]=Math.min(pick,notPick);
     }
 }

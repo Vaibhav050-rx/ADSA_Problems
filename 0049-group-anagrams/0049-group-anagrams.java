3ffee846dc1bf699c64ae31be5370d4dd27adc21
Vaibhav050-rx <vaibhavjain7925@gmail.com>
2026-06-01 14:34:40 +0530

    Time: 7 ms (73.15%), Space: 49.4 MB (91.19%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,28 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> mp = new HashMap<>();
-        for(int i =0;i<strs.length;i++){
-            String freqString = getFreq(strs[i]);
-            if(!mp.containsKey(freqString)){
-                // make a new list 
-                mp.put(freqString, new ArrayList<>());
+        for(String str:strs){
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String sorted = new String(arr);
+            if(!mp.containsKey(sorted)){
+                // create a new list
+                mp.put(sorted,new ArrayList<>());
             }
-            mp.get(freqString).add(strs[i]);
+            mp.get(sorted).add(str);
         }
         return new ArrayList<>(mp.values());
-    }
-    public static String getFreq(String strs){
-        int freq[] = new int[26];
-        for(char ch:strs.toCharArray()){
-            freq[ch-'a']++;    
-        }
-        StringBuilder sb= new StringBuilder();
-        char ch ='a';
-        for(int count:freq){
-            sb.append(ch);
-            sb.append(count);
-            ch++;
-        }
-        return sb.toString();
     }
 }

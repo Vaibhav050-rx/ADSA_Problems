d8b7fdeb867bcb589821faee2371b15e899f5bdd
Vaibhav050-rx <vaibhavjain7925@gmail.com>
2026-06-06 20:54:46 +0530

    Time: 7 ms (68.73%), Space: 46.2 MB (81.21%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -2,24 +2,22 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
-        int[][] dp = new int[n][m];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
-        int lcs = LCS(word1, word2, n - 1, m - 1, dp);
-        return n + m - 2 * lcs;
+        int[][]dp=new int[n+1][m+1];
+        int lcs=LCS(word1,word2,dp);
+        return n+m-2*lcs;
     }
-
-    public static int LCS(String s1, String s2,int i, int j,int[][] dp) {
-        if (i < 0 || j < 0) {
-            return 0;
+    public static int LCS(String s1,String s2,int[][]dp){
+        int n = s1.length();
+        int m = s2.length();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        if (s1.charAt(i) == s2.charAt(j)) {
-            return dp[i][j] =1 + LCS(s1, s2, i - 1, j - 1, dp);
-        }
-        return dp[i][j] = Math.max(LCS(s1, s2, i - 1, j, dp),LCS(s1, s2, i, j - 1, dp));        
+        return dp[n][m];
     }
 }

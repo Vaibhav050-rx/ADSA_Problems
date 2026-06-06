cd473b4b72c5c867565604ee608fbfe517f9cc66
Vaibhav050-rx <vaibhavjain7925@gmail.com>
2026-06-06 20:43:39 +0530

    Time: 7 ms (68.73%), Space: 46.5 MB (45.76%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,23 +1,25 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int n =word1.length();
+        int n = word1.length();
         int m = word2.length();
-        int[][]dp=new int[n][m];
-        for(int[]row:dp){
-            Arrays.fill(row,-1);
+        int[][] dp = new int[n][m];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        return solve(word1,word2,n-1,m-1,dp);
+        int lcs = LCS(word1, word2, n - 1, m - 1, dp);
+        return n + m - 2 * lcs;
     }
-    public static int solve(String s1, String s2,int i ,int j ,int[][]dp){
-        // base case
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)){
-            dp[i][j]=solve(s1,s2,i-1,j-1,dp);
-        }else{
-            dp[i][j]=Math.min(1+solve(s1,s2,i-1,j,dp),1+solve(s1,s2,i,j-1,dp));
+
+    public static int LCS(String s1, String s2,int i, int j,int[][] dp) {
+        if (i < 0 || j < 0) {
+            return 0;
         }
-        return dp[i][j];
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if (s1.charAt(i) == s2.charAt(j)) {
+            return dp[i][j] =1 + LCS(s1, s2, i - 1, j - 1, dp);
+        }
+        return dp[i][j] = Math.max(LCS(s1, s2, i - 1, j, dp),LCS(s1, s2, i, j - 1, dp));        
     }
 }

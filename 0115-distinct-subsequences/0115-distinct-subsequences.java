a200879d11abbfb5b58587523629b974cf975e49
Vaibhav050-rx <vaibhavjain7925@gmail.com>
2026-06-08 00:55:30 +0530

    Time: 20 ms (56.14%), Space: 66.4 MB (5.34%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,20 +3,18 @@ class Solution {
         int n = s.length();
         int m =t.length();
         long[][]dp=new long[n+1][m+1];
-        for(long[]row:dp){
-            Arrays.fill(row,-1);
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
         }
-        return (int)solve(s,t,n,m,dp);
-    }
-    public static long solve(String s, String t,int i,int j,long[][]dp){
-        if(j==0) return 1;
-        if(i==0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s.charAt(i-1)==t.charAt(j-1)){
-            dp[i][j]=solve(s,t,i-1,j-1,dp)+solve(s,t,i-1,j,dp);
-        }else{
-            dp[i][j]=solve(s,t,i-1,j,dp);
+        for(int i=1;i<=n;i++){
+            for(int j =1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                  dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
         }
-        return dp[i][j];
+        return (int)dp[n][m];
     }
 }

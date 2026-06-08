3543d756c194b26e0c0f37eda961af4499383de8
Vaibhav050-rx <vaibhavjain7925@gmail.com>
2026-06-08 23:20:13 +0530

    Time: 55 ms (20.98%), Space: 47.5 MB (54.45%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,44 +1,48 @@
 class Solution {
     public boolean isMatch(String s, String p) {
+
         int n = s.length();
-        int m =p.length();
-        int[][]dp = new int[n][m];
-        for(int[]row:dp){
-            Arrays.fill(row,-1);
-        }
-        return solve(s, p, n - 1, m - 1, dp);
-    }
-    public static boolean solve(String s, String p,int i,int j,int[][]dp){
-        if (i < 0 && j < 0) return true;
-        if (j < 0 && i >= 0) return false;
-        if (i < 0 && j >= 0) {
-            for (int k = 0; k <= j; k++) {
-                if (p.charAt(k) != '*') return false;
+        int m = p.length();
+
+        boolean[][] dp = new boolean[n + 1][m + 1];
+
+        dp[0][0] = true;
+
+        for (int j = 1; j <= m; j++) {
+            boolean flag = true;
+
+            for (int k = 1; k <= j; k++) {
+                if (p.charAt(k - 1) != '*') {
+                    flag = false;
+                    break;
+                }
             }
-            return true;
+
+            dp[0][j] = flag;
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j] == 1;
-        }
-        if(s.charAt(i)==p.charAt(j)||p.charAt(j)=='?'){
-            if(solve(s,p,i-1,j-1,dp)){
-                dp[i][j]=1;
-                return true;
-            }else{
-                dp[i][j]=0;
-                return false;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= m; j++) {
+
+                if (s.charAt(i - 1) == p.charAt(j - 1)
+                        || p.charAt(j - 1) == '?') {
+
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+
+                else if (p.charAt(j - 1) == '*') {
+
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+
+                else {
+
+                    dp[i][j] = false;
+                }
             }
         }
-        if(p.charAt(j)=='*'){
-            if(solve(s,p,i-1,j,dp)||solve(s,p,i,j-1,dp)){
-                dp[i][j]=1;
-                return true;
-            }else{
-                dp[i][j]=0;
-                return false;
-            }
-        }
-        dp[i][j]=0;
-        return false;
+
+        return dp[n][m];
     }
 }

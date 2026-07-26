dfd2eb204d11f1b23b16b0ebd07c89f15ed83d90
Vaibhav050-rx <vaibhavjain7925@gmail.com>
2026-07-26 18:33:33 +0530

    Time: 0 ms (100%), Space: 49.7 MB (13.46%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -1,23 +1,26 @@
 class Solution {
     public int countNodes(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        int sum =0;
-        while(!queue.isEmpty()){
-            if(root == null) return 0;
-            int size=queue.size();
-            sum+=size;
-            for(int i =0;i<size;i++){
-                TreeNode node = queue.poll();
-                if(node.left!=null){
-                    queue.offer(node.left);
-                }
-                if(node.right!=null){
-                    queue.offer(node.right);
-                }
-            }
+        if(root == null){
+            return 0;
         }
-        return sum;
-            
+        int leftHeight = getLeftHeight(root);
+        int rightHeight = getRightHeight(root);
+        if(leftHeight == rightHeight){
+            return  (int)Math.pow(2, leftHeight) - 1;
+        }
+        return 1+countNodes(root.left)+countNodes(root.right);
+    }
+    public int getLeftHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + getLeftHeight(root.left);
+    }
+
+    public int getRightHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + getRightHeight(root.right);
     }
 }

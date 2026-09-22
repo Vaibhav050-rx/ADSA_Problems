dc4a29ebecf75138765d694ea4a6ba8cb098f30c
Vaibhav050-rx <vaibhavjain7925@gmail.com>
2026-09-22 12:09:30 +0530

    Time: 7 ms (66.29%), Space: 44.2 MB (48.46%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,28 +1,22 @@
 class Solution {
-    public static boolean isFreqSame(int [] freq1,int[]freq2){
-        for(int i =0;i<26;i++){
-            if(freq1[i]!=freq2[i]) return false;
-        }
-        return true;
-    }
     public boolean checkInclusion(String s1, String s2) {
-        int [] freq = new int[26];
-        for(int i =0;i<s1.length();i++){
-            char ch = s1.charAt(i);
-            freq[ch-'a']++;
+        int[]s1freq = new int[26];
+        int[]s2freq = new int[26];
+        for(int j =0;j<s1.length();j++){
+            char c = s1.charAt(j);
+            s1freq[c-'a']++;
         }
-        int left=0;
-        int [] freq2=new int[26];
-        for(int right =0;right<s2.length();right++){
-            // add freq
-            char c = s2.charAt(right);
-            freq2[c-'a']++;
-            if(right-left+1>s1.length()){
-                char remove= s2.charAt(left);
-                freq2[remove-'a']--;
-                left++;
+        int j =0;
+        for(int i =0 ;i<s2.length();i++){
+            char ch =s2.charAt(i);
+            s2freq[ch-'a']++;
+            if(i-j+1 == s1.length()){
+                if(Arrays.equals(s1freq,s2freq)){
+                    return true;
+                }
+                s2freq[s2.charAt(j)-'a']--;
+                j++;
             }
-            if(isFreqSame(freq,freq2)) return true;
         }
         return false;
     }
